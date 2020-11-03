9612683cd39c75ecf9acce819388e0f916750311
abx393 <abandari99@gmail.com>
2020-11-02 22:10:22 -0800

    adding problem CommonDivisors

---
+++ scripts/CommonDivisors/CommonDivisors.java
@@ -0,0 +1,87 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class CommonDivisors {
+	public static long MOD = 1000000007L;
+  
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.next();
+		String s2 = sc.next();
+		sc.close();
+
+		long h = hash(s1);
+		long hash = 0;
+		HashSet<Long> h1 = new HashSet<Long>();
+		for (int i = 0; i < s1.length(); i++){
+			long add = ((s1.charAt(i) - 96) * exp(27, i)) % MOD;
+			hash += add;
+			hash %= MOD;
+			long newhash = 0;
+			if (s1.length() % (i + 1) == 0) {
+				int factor = s1.length() / (i + 1);
+				for (int j = 0; j < factor; j++){
+					long tmp = (hash * exp(27, (i+1) * j)) % MOD;
+					newhash += tmp;
+					newhash %= MOD;
+				}
+				if (newhash == h){
+					h1.add(hash);
+				}
+			}
+		}
+
+		int count = 0;
+		h = hash(s2);
+		hash = 0;
+		
+		for (int i = 0; i < s2.length(); i++){
+			long add = ((s2.charAt(i) - 96) * exp(27, i)) % MOD;
+			hash += add;
+			hash %= MOD;
+			long newhash = 0;
+			if (s2.length() % (i + 1) == 0) {
+				int factor = s2.length() / (i + 1);
+				for (int j = 0; j < factor; j++){
+					long tmp = (hash * exp(27, (i + 1) * j)) % MOD;
+					newhash += tmp;
+					newhash %= MOD;
+				}
+				if (newhash == h){
+					//System.out.println(s2.substring(0, i+1));
+					int size = h1.size();
+					h1.add(hash);
+					if (h1.size() == size) count++;
+				}
+			}
+		}
+		//System.out.println(h1.size());
+		System.out.println(count);
+	}
+
+	public static long hash(String s){
+		long hash = 0;
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			long add = ((c - 96) * exp(27, i)) % MOD;
+			hash += add;
+			hash %= MOD;
+		}
+		return hash;
+	}
+
+	public static long exp(long base, int power){
+		long ans = 1;
+		if (power==0) ans = 1;
+		else if (power % 2 == 0){
+			ans = exp(base, power / 2);
+			ans *= ans;
+			ans %= MOD;
+		} else {
+			ans *= base;
+			ans *= exp(base, power - 1);
+			ans %= MOD;
+		}
+		return ans;
+	}
+}
---
+++ scripts/CommonDivisors/CommonDivisors.md
@@ -0,0 +1,19 @@
+# Source
+https://codeforces.com/problemset/problem/182/D
+
+# Problem Statement
+Vasya has recently learned at school what a number's divisor is and decided to determine a string's divisor. Here is what he came up with.
+
+String a is the divisor of string b if and only if there exists a positive integer x such that if we write out string a consecutively x times, we get string b. For example, string "abab" has two divisors — "ab" and "abab".
+
+Now Vasya wants to write a program that calculates the number of common divisors of two strings. Please help him.
+
+# Input
+The first input line contains a non-empty string s1.
+
+The second input line contains a non-empty string s2.
+
+Lengths of strings s1 and s2 are positive and do not exceed 105. The strings only consist of lowercase Latin letters.
+
+# Output
+Print the number of common divisors of strings s1 and s2.
